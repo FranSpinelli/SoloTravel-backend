83ae5eeb77718d919d8b7112352081150386b20e
Francisco Nahuel Svrznjak Spinelli <franciscospinelli98@gmail.com>
2021-09-19 15:36:55 -0300

    Mas configuraciones generales del proyecto
    	+ se agregó Lombok
    	+ se agregó configuracion para utilizar h2
    	+ se excluyó la configuracion automatica realizada por spring security al levantar la aplicacion

--- src/main/java/ar/edu/unq/solotravel/backend/api/BackendSoloTravelApiApplication.java
+++ src/main/java/ar/edu/unq/solotravel/backend/api/BackendSoloTravelApiApplication.java
@@ -2,8 +2,9 @@ package ar.edu.unq.solotravel.backend.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class BackendSoloTravelApiApplication {
 
 	public static void main(String[] args) {
