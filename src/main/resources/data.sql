428cae360d10772e47166ecc7011af1437a63e5d
Francisco Nahuel Svrznjak Spinelli <franciscospinelli98@gmail.com>
2021-09-19 15:41:08 -0300

    Se agregó scrip de carga de datos para la bbdd h2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO trips (id, name, destination, description, price, start_date, end_date, category) VALUES
+    (1, 'San Martin de los Andes y la ruta de los 7 lagos', 'San Martin De Los Andes', 'Una descripcion que hay q pensar mejor', 20000.0, '2022-01-03', '2022-01-09', 'CATEGORIA1'),
+    (2, 'El calafate: tierra de glaciares', 'El Calafate', 'Una descripcion que hay que pensar mejor', 15000.0, '2022-01-10', '2022-01-16', 'CATEGORIA2'),
+    (3, 'Mucho Trekking en Bariloche', 'Bariloche', 'Una descripcion que hay que pensar mejor', 20000.0, '2022-01-17', '2022-01-23', 'CATEGORIA3'),
+    (4, 'El chalten: capital nacional del trekking', 'El Chalten', 'Una descripcion que hay que pensar mejor', 25000.0, '2022-01-24', '2022-01-30', 'CATEGORIA1'),
+    (5, 'Ushuaia: la ciudad del fin del mundo', 'Ushuaia', 'Una descripcion que hay que pensar mejor', 30000.0, '2022-01-31', '2022-02-06', 'CATEGORIA2'),
+    (6, 'Jujuy: tierra de colores y tradiciones', 'Jujuy', 'Una descripcion que hay que pensar mejor', 25000.0, '2022-02-07', '2022-02-13', 'CATEGORIA3'),
+    (7, 'Las cataratas del iguazu: la 7ma maravilla del mundo', 'Cataratas del Iguazu', 'Una descripcion que hay que pensar mejor', 25000.0, '2022-01-04', '2022-01-11', 'CATEGORIA1'),
+    (8, 'La Rioja y el parque nacional talampaya', 'La Rioja', 'Una descripcion que hay que pensar mejor', 20000.0, '2022-01-18', '2022-01-25', 'CATEGORIA2'),
+    (9, 'Escapada a Mar del Plata', 'Mar del Plata', 'Una descripcion que hay que pensar mejor', 15000.0, '2022-01-06', '2022-01-09', 'CATEGORIA1'),
+    (10, 'Escapada a Pinamar', 'Pinamar', 'Una descripcion que hay que pensar mejor', 15000.0, '2022-01-06', '2022-01-09', 'CATEGORIA1'),
+    (11, 'Mendoza: montañas, historia, cultura y mucho vino!', 'Mendoza', 'Una descripcion que hay que pensar mejor', 25000.0, '2022-02-03', '2022-02-015', 'CATEGORIA2');
