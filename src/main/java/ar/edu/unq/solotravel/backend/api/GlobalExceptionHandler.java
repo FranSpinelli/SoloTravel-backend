21c3ee4d57134230d54c80b41a044c7f246c397d
Francisco Nahuel Svrznjak Spinelli <franciscospinelli98@gmail.com>
2021-09-26 22:23:47 -0300

    errorDto was created

--- src/main/java/ar/edu/unq/solotravel/backend/api/GlobalExceptionHandler.java
+++ src/main/java/ar/edu/unq/solotravel/backend/api/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package ar.edu.unq.solotravel.backend.api;
 
+import ar.edu.unq.solotravel.backend.api.dtos.ErrorResponseDto;
 import ar.edu.unq.solotravel.backend.api.exceptions.NoSuchElementException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,6 +11,6 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity handleWrongApiKeyException(NoSuchElementException noSuchElementException){
-        return ResponseEntity.status(404).body(noSuchElementException.getMessage());
+        return ResponseEntity.status(404).body(new ErrorResponseDto(noSuchElementException.getMessage()));
     }
 }
