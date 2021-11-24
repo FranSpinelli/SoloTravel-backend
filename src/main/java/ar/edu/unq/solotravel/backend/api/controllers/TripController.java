4f794cf8d1f55b7b592e41aee68d24b8a82cdf15
Nicolas Ruiz <miguel.ruiz@uala.com.ar>
2021-11-24 01:10:04 -0300

    Added date filter in trip searching

--- src/main/java/ar/edu/unq/solotravel/backend/api/controllers/TripController.java
+++ src/main/java/ar/edu/unq/solotravel/backend/api/controllers/TripController.java
@@ -1,11 +1,16 @@
 package ar.edu.unq.solotravel.backend.api.controllers;
 
+import ar.edu.unq.solotravel.backend.api.dtos.SearchTripParamsDto;
 import ar.edu.unq.solotravel.backend.api.exceptions.NoSuchElementException;
 import ar.edu.unq.solotravel.backend.api.security.ValidateInternalJwt;
 import ar.edu.unq.solotravel.backend.api.services.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @CrossOrigin
 @RestController
@@ -16,8 +21,14 @@ public class TripController {
     private TripService tripService;
 
     @GetMapping()
-    public ResponseEntity getAllTrips(@RequestParam(required = false) String name) {
-        return ResponseEntity.ok().body(tripService.getAllTrips(name));
+    public ResponseEntity getAllTrips(
+            @RequestParam(required = false) String destination,
+            @RequestParam(required = false) String date) {
+        LocalDate searchDate = date == null ? LocalDate.now() : LocalDate.parse(date.substring(0, 10));
+        if (searchDate.isBefore(LocalDate.now()))
+            searchDate = LocalDate.now();
+        SearchTripParamsDto paramsDto = new SearchTripParamsDto(destination, searchDate);
+        return ResponseEntity.ok().body(tripService.getAllTrips(paramsDto));
     }
 
     @ValidateInternalJwt
